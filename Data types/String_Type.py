abb9235e3c4907a8f1e2795f7f38ec27e7349bc4
maoyed <qwermoaid17@gmail.com>
2026-04-27 14:34:36 +0200

    Some modifications to the titles and adding a new exercise in the datatypes folder.

---
+++ Data types/String_Type.py
@@ -0,0 +1,47 @@
+import pymysql as sql
+
+try:
+
+    db = sql.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        charset='utf8mb4'
+    )
+
+    with open(r"C:\Users\qwerm\Pictures\Camera Roll\WIN_20260425_19_36_53_Pro.jpg", "rb") as f:
+        img = f.read()
+
+    cr = db.cursor()
+
+    cr.execute("CREATE DATABASE IF NOT EXISTS String_Type")
+
+    cr.execute("USE String_Type")
+
+    cr.execute("CREATE TABLE IF NOT EXISTS tbl (id INT AUTO_INCREMENT PRIMARY KEY, string CHAR(1), name VARCHAR(100))")
+
+    # cr.execute("ALTER TABLE tbl ADD COLUMN string2 TEXT")
+
+    # cr.execute("ALTER TABLE tbl ADD COLUMN string3 BLOB")
+
+    # cr.execute("ALTER TABLE tbl ADD COLUMN string4 ENUM('python', 'java', 'c++', 'html' )")
+
+    cr.execute("INSERT INTO tbl (string4) VALUES (%s)", ("python",))
+
+    # cr.execute("ALTER TABLE tbl ADD COLUMN string5 SET('google', 'Brave', 'Firefox', 'Edge')")
+
+    cr.execute("INSERT INTO tbl (string5) VALUES (%s)", ("google,Brave",))
+
+
+    cr.execute("INSERT INTO tbl (string2, string3) VALUES (%s, %s)", ("This is a text", img))
+
+    for i in range(10):
+
+        cr.execute("INSERT INTO tbl (String, name) VALUES (%s, %s)", (i, i))
+    
+    db.commit()
+
+    db.close()
+
+except sql.Error as er:
+    print("Error ", er)    
