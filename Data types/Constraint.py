9d5f6d40e922f54824bdedceff1b93cb8acd9cd2
maoyed <qwermoaid17@gmail.com>
2026-05-04 15:17:10 +0200

    First, I added a simple project to the sidebar. Second, I practiced the 'NOT NULL' lesson on it.

---
+++ Data types/Constraint.py
@@ -0,0 +1,28 @@
+import pymysql as sql
+
+try:
+
+    db = sql.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        charset='utf8mb4'
+    )
+
+    cr = db.cursor()
+
+    cr.execute("USE moayed")
+
+    # cr.execute("INSERT INTO students (maoyed, id, name, email, password, username) VALUES (%s, %s, %s, %s, %s, %s)", (None , None, None, None, None, None))
+
+    cr.execute("ALTER TABLE students CHANGE COLUMN username username VARCHAR(255) NOT NULL")
+
+    cr.execute("ALTER TABLE students ADD COLUMN ahmed VARCHAR(255) NOT NULL")
+
+    db.commit()
+
+    db.close()
+
+except sql.Error as er:
+
+    print("Error ", er)
