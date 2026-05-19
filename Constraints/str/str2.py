6e474e153dd30a6608e3f26b027df12d178cd7df
maoyed <qwermoaid17@gmail.com>
2026-05-19 11:17:44 +0200

    feat: demonstrate difference between LENGTH() and CHAR_LENGTH()
    - Tested string manipulation functions with multi-byte characters.
    - Used the Euro symbol (€) to analyze byte size vs character count.
    - Handled utf8mb4 encoding outputs correctly using Python.

---
+++ Constraints/str/str2.py
@@ -0,0 +1,55 @@
+import pymysql as sql
+
+try:
+
+    db = sql.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        charset='utf8mb4'
+    )
+
+    cr = db.cursor()
+
+    cr.execute("USE tests")
+
+    def INSERT_users(cr):
+
+        cr.execute("INSERT INTO users (text) VALUES (%s)", ("€",))
+
+    def LEN(cr):
+
+        cr.execute("SELECT text ,LENGTH(text) AS count FROM users")
+
+        result = cr.fetchall()
+
+        for row in result:
+
+            print(f"Text: {row[0]}, Length: {row[1]}")
+
+    def CHAR_LEN(cr):
+
+        cr.execute("SELECT text ,CHAR_LENGTH(text) AS count FROM users")
+
+        result = cr.fetchall()
+
+        for row in result:
+
+            print(f"Text: {row[0]}, Length: {row[1]}")
+
+    CHAR_LEN(cr)
+
+    # INSERT_users(cr)
+
+    LEN(cr)
+
+    db.commit()
+
+except sql.Error as er:
+
+    print("Error ", er)
+
+finally:
+
+    if db:
+        db.close()
